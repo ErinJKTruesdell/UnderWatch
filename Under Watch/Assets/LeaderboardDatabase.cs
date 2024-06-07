1a23404b58d12180ab262b13658b3998d5b44bad
Erin J.K. Truesdell <nern.strudel@gmail.com>
2024-06-07 12:42:23 -0400

    create scrolling leaderboard from database

--- Under Watch/Assets/LeaderboardDatabase.cs
+++ Under Watch/Assets/LeaderboardDatabase.cs
@@ -2,81 +2,135 @@ using Mopsicus.InfiniteScroll;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+public class  pointsData
+{
+    public string username;
+    public int points;
+    public string profUrl;
+    public pointsData(string un, string pts, string url)
+    {
+        username = un; points = int.Parse(pts); profUrl = url;
+    }
+}
 public class LeaderboardDatabase : MonoBehaviour
 {
 
 
-    [SerializeField]
-    private InfiniteScroll Scroll;
+    public GameObject leaderboardItemPrefab;
 
-    [SerializeField]
-    private int Count = 7;
+    public GameObject leaderboardloading;
 
-    [SerializeField]
-    private int PullCount = 7;
+    string rootURL = "https://erinjktruesdell.com/";
 
-    private List<int> _list = new List<int>();
+    bool isWorking;
 
-    public Texture sampleTex;
-
-
-    public int screenWidth = 500;
+    public Transform gridObj;
 
     void Start()
     {
 
-        screenWidth = Screen.width;
-        Debug.Log("SCREEN WITDTH: " + screenWidth);
-        Scroll.OnFill += OnFillItem;
-        Scroll.OnHeight += OnHeightItem;
-        Scroll.OnPull += OnPullItem;
-        for (int i = 0; i < Count; i++)
+
+    }
+
+    private void Awake()
+    {
+        if (leaderboardloading != null)
         {
-            _list.Add(i);
+            leaderboardloading.SetActive(true);
         }
-        Scroll.InitData(_list.Count);
+        StartCoroutine(StockLeaderBoard());
     }
 
-    void OnFillItem(int index, GameObject item)
+    IEnumerator StockLeaderBoard()
     {
-        //database code here
-        ListItem li = item.GetComponentInChildren<ListItem>();
-        //sfd.getNextPost(li.photoImg, li.profImage, li.unText);
-        //item.GetComponentInChildren<Text>().text = sfo.username;
-        //item.GetComponentInChildren<RawImage>().texture = sfo.photo;
-    }
+        // get data from server
+        WWWForm form = new WWWForm();
+        form.AddField("s", "s"); //dummy data
 
-    int OnHeightItem(int index)
-    {
-        return 800;
-    }
+        List<pointsData> allPoints = new List<pointsData>();
 
-    void OnPullItem(InfiniteScroll.Direction direction)
-    {
-        int index = _list.Count;
-        if (direction == InfiniteScroll.Direction.Top)
+        using (UnityWebRequest www = UnityWebRequest.Post(rootURL + "/get-next-leaderboard.php", form))
         {
-            for (int i = 0; i < PullCount; i++)
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
             {
-                _list.Insert(0, index);
-                index++;
+                //yay show the picture
+                //errorMessage = www.error;
+                string errorMessage = www.error;
+                Debug.Log(errorMessage);
+
+                Debug.Log("Data get error, releasing queue");
+                isWorking = false;
             }
+            else
+            {
+                //return null
+                string responseText = www.downloadHandler.text;
+                string[] userChunks = responseText.Split('%');
+                foreach (string chunk in userChunks)
+                {
+                    string[] userData = chunk.Split('|');
+                    if (userData.Length > 2) // trim off that last empty bit
+                    {
+                        //username, points value, prof URL
+                        //Debug.Log(userData[0] + ": " + userData[1] + " points |" + userData[2]);
+                        allPoints.Add(new pointsData(userData[0], userData[1], userData[2]));
+
+                    }
+                }
+
+
+                allPoints.Sort((x, y) => x.points.CompareTo(y.points));
+                allPoints.Reverse();
+
+                gridObj.GetComponent<RectTransform>().sizeDelta = new Vector2(700, allPoints.Count * 200);
+
+                foreach(pointsData i in allPoints)
+                {
+
+                    GameObject leaderBoardUserItem = Instantiate(leaderboardItemPrefab) as GameObject;
+                    leaderBoardUserItem.transform.parent = gridObj;
+                    LeaderboardItem li = leaderBoardUserItem.GetComponent<LeaderboardItem>();
+
+                    li.usernameText.text = i.username;
+                    li.pointsText.text = i.points.ToString() + " points";
+
+                    //downlaod prof img
+                    StartCoroutine(downloadImageFromURL(rootURL + i.profUrl, li.profilePic));
+                }
+
+            }
+            //break data into chunks
+
+            //iterate through chunks, downloading pfolie pics and creating prefabs
+
+
+            //close loading screen, if one implemented
+            if (leaderboardloading != null)
+            {
+                leaderboardloading.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator downloadImageFromURL(string url1, RawImage image1)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url1);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
         }
         else
         {
-            for (int i = 0; i < PullCount; i++)
-            {
-                _list.Add(index);
-                index++;
-            }
+            image1.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
         }
-        Scroll.ApplyDataTo(_list.Count, PullCount, direction);
+
     }
 
-    public void SceneLoad(int index)
-    {
-        SceneManager.LoadScene(index);
-    }
 }
