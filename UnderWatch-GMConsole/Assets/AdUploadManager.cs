d7975f9c9fcb0ea8632ca756c9714cf6e4126b60
Erin J.K. Truesdell <nern.strudel@gmail.com>
2024-07-16 17:22:21 -0400

    most of ad upload is done

--- UnderWatch-GMConsole/Assets/AdUploadManager.cs
+++ UnderWatch-GMConsole/Assets/AdUploadManager.cs
@@ -6,14 +6,23 @@ using UnityEngine;
 using static OnlineMapsGPXObject;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using SimpleFileBrowser;
+using System.IO;
 
 public class AdUploadManager : MonoBehaviour
 {
+
+    public TabButton adUploadTab;
 
     string[] filepaths;
     public TMP_Text errorText;
     public AdTimestampManager atm;
     bool isWorking;
+
+    public TMP_Text adRateText;
+    public TMP_Dropdown adRateDropdown;
+
+    public TMP_Text numadstext;
 
     string rootURL = "https://erinjktruesdell.com/";
 
@@ -21,13 +30,43 @@ public class AdUploadManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        if (adUploadTab != null)
+        {
+            adUploadTab.Activated += new TabButton.ActivatedHandler(OnAdUploadActivated);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public void startAdSelect()
     {
+        StartCoroutine(ShowLoadDialogCoroutine());
+    }
 
+    IEnumerator ShowLoadDialogCoroutine()
+    {
+        // Show a load file dialog and wait for a response from user
+        // Load file/folder: file, Allow multiple selection: true
+        // Initial path: default (Documents), Initial filename: empty
+        // Title: "Load File", Submit button text: "Load"
+        FileBrowser.SetFilters(false, new string[] { ".png" });
+        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Files, true, null, null, "Select Files", "Load");
+
+        // Dialog is closed
+        // Print whether the user has selected some files or cancelled the operation (FileBrowser.Success)
+        Debug.Log(FileBrowser.Success);
+
+        if (FileBrowser.Success)
+            OnFilesSelected(FileBrowser.Result); // FileBrowser.Result is null, if FileBrowser.Success is false
+    }
+
+    void OnFilesSelected(string[] filePaths)
+    {
+        filepaths = filePaths;
+        numadstext.text = filePaths.Length + " ad files selected.";
+    }
+    public void OnAdUploadActivated(TabButton t, EventArgs e)
+    {
+        refreshAdRate();
     }
 
     public void startUpload()
@@ -65,6 +104,97 @@ public class AdUploadManager : MonoBehaviour
         isWorking = false;
     }
 
+    public void refreshAdRate()
+    {
+        StartCoroutine(getAdRate());
+
+
+    }
+
+    public IEnumerator setAdRate() {
+
+        int selectedAdRate = adRateDropdown.value;
+        // get data from server
+        WWWForm form = new WWWForm();
+        Debug.Log("Selected ad rate to upload: " + selectedAdRate);
+        form.AddField("adRate", selectedAdRate.ToString()); 
+
+
+        using (UnityWebRequest www = UnityWebRequest.Post(rootURL + "/set_ad_rate.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                //yay show the picture
+                //errorMessage = www.error;
+                string errorMessage = www.error;
+                Debug.Log(errorMessage);
+            }
+            else
+            {
+                //return null
+                string responseText = www.downloadHandler.text;
+
+
+                Debug.Log("Response: " + responseText);
+                selectedAdRate = int.Parse(responseText);
+                adRateText.text = "Ads are currently shown every " + selectedAdRate.ToString() + " posts";
+
+
+            }
+        }
+
+    }
+
+    public IEnumerator getAdRate()
+    {
+        //get from server
+
+        int selectedAdRate = -1;
+        // get data from server
+        WWWForm form = new WWWForm();
+        form.AddField("s", "s"); //dummy data
+
+
+        using (UnityWebRequest www = UnityWebRequest.Post(rootURL + "/get_ad_rate.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                //yay show the picture
+                //errorMessage = www.error;
+                string errorMessage = www.error;
+                Debug.Log(errorMessage);
+            }
+            else
+            {
+                //return null
+                string responseText = www.downloadHandler.text;
+                Debug.Log("Response: " + responseText);
+                selectedAdRate = int.Parse(responseText);
+
+
+
+            }
+        }
+
+        adRateDropdown.ClearOptions();
+        List<string> adRateOptions = new List<string>();
+        for (int i = 0; i < 21; i++)
+        {
+            adRateOptions.Add(i.ToString());
+        }
+        adRateDropdown.AddOptions(adRateOptions);
+        adRateDropdown.value = selectedAdRate;
+        adRateText.text = "Ads are currently shown every " + selectedAdRate.ToString() + " posts";
+    }
+
+    public void sendUpdatedAdRate()
+    {
+        StartCoroutine(setAdRate());
+    }
 
     public IEnumerator uploadAllAds()
     {
