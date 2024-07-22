9117982b16ac520c7310442e0a2ee8a7c4a14268
EmeryKJ <77759693+EmeryKJ@users.noreply.github.com>
2024-07-22 13:19:49 -0400

    Fixed post UI scaling issues

--- Under Watch/Assets/Scripts/Demo2.cs
+++ Under Watch/Assets/Scripts/Demo2.cs
@@ -58,7 +58,7 @@ public class Demo2 : MonoBehaviour
 
     int OnHeightItem(int index)
     {
-        return 800;
+        return 509;
     }
 
     void OnPullItem(InfiniteScroll.Direction direction)
